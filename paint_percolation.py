60f31b4fea9ad69a7d78a2e8d1110e17e06521fb
kerrek8 <kerrek8@gmail.com>
2024-03-25 18:06:50 +0500

    немного поправил отображение треугольной сетки

--- paint_percolation.py
+++ paint_percolation.py
@@ -1,8 +1,5 @@
-﻿from PyQt5.QtGui import QBrush, QPen, QPainter, QColor, QFont, QPainterPath
-from PyQt5.QtCore import Qt
-from PyQt5.QtCore import QLineF, QPointF, QRectF
-from decimal import Decimal
-import math
+﻿from PyQt5.QtCore import QPointF
+from PyQt5.QtGui import QColor
 
 
 def paint_percolation_cell(painter, percolation, color_lst):
@@ -131,15 +128,15 @@ def paint_percolation_triangle(painter, percolation, color_lst):
                                 start_y + 2 * (i - 1) * radius + radius / 2),
                         radius / 2, radius / 2)
                     if percolation.a[i - 1][j - 1] == percolation.a[i][j]:
-                        painter.drawLine(QPointF(start_x + 2 * (j - 1) * radius + radius / 2,
-                                                 start_y + 2 * (i - 1) * radius),
-                                         QPointF(start_x + 2 * (j - 1) * radius - radius - radius / 2,
-                                                 start_y + 2 * (i - 1) * radius - radius))
+                        painter.drawLine(QPointF(start_x + 2 * (j - 1) * radius + radius / 6,
+                                                 start_y + 2 * (i - 1) * radius + radius * 0.130),
+                                         QPointF(start_x + 2 * (j - 1) * radius - radius / 2 - 0.640 * radius,
+                                                 start_y + 2 * (i - 1) * radius - 1.140 * radius))
                     if percolation.a[i - 1][j + 1] == percolation.a[i][j]:
-                        painter.drawLine(QPointF(start_x + 2 * (j - 1) * radius + radius / 2,
-                                                 start_y + 2 * (i - 1) * radius),
-                                         QPointF(start_x + 2 * (j - 1) * radius + 2 * radius + radius / 2,
-                                                 start_y + 2 * (i - 1) * radius - radius))
+                        painter.drawLine(QPointF(start_x + 2 * (j - 1) * radius + radius / 1.220,
+                                                 start_y + 2 * (i - 1) * radius + 0.130 * radius),
+                                         QPointF(start_x + 2 * (j - 1) * radius + radius / 2 + 1.640 * radius,
+                                                 start_y + 2 * (i - 1) * radius - 1.140 * radius))
                     if (j - 2) >= 0 and percolation.a[i][j - 2] == percolation.a[i][j]:
                         painter.drawLine(QPointF(start_x + 2 * (j - 1) * radius,
                                                  start_y + 2 * (i - 1) * radius + radius / 2),
